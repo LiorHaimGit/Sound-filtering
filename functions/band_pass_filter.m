52652e9de786db97b8f706886af0b904313edbcc
LiorHaimGit <126193345+LiorHaimGit@users.noreply.github.com>
2024-03-04 13:27:12 -0800

    Update band_pass_filter.m

--- functions/band_pass_filter.m
+++ functions/band_pass_filter.m
@@ -1,1 +1,9 @@
+function filtered_data = band_pass_filter(data, sample_rate, cutoff_freq_low, cutoff_freq_high, order)
 
+nyquist_freq = sample_rate / 2; % Nyquist frequency
+
+% Design the Butterworth bandpass filter
+[b, a] = butter(order, [cutoff_freq_low, cutoff_freq_high] / nyquist_freq, 'bandpass');
+
+% Apply the filter to the audio data
+filtered_data = filter(b, a, data);
